472e763eb8795203f7a24e3e08591a193976ca96
DarthPointer <al.kiriackin@yandex.ru>
2023-09-30 16:23:47 +0600

    Added a working example with generic property nested into a generic container.

---
+++ Source/InferringGenericTypeparam/InferringGenericTypeparam/Model/Container.cs
@@ -0,0 +1,8 @@
+﻿namespace InferringGenericTypeparam.Model
+{
+    public class Container<TClass, TValue>
+        where TClass : GenericClass<TValue>
+    {
+        public required TClass Instance { get; init; }
+    }
+}
